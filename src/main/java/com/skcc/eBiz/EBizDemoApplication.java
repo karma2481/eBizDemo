d9161f48b0978189857c5f8f67802ee0e635ce3c
karma2481 <karma2481@gmail.com>
2019-09-10 15:17:39 +0900

    init push

---
+++ src/main/java/com/skcc/eBiz/EBizDemoApplication.java
@@ -0,0 +1,23 @@
+package com.skcc.eBiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class EBizDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBizDemoApplication.class, args);
+	}
+	
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello(){
+        return "hello world 테스트입니다.";
+    }	
+
+}
